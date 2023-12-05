5901a1aaa713010c028cabaa92e9e83a74df9da9
RoCalderon <rocalderon1405@hotmail.com>
2023-12-05 02:04:31 -0600

    Clase 36 Listado personas

--- listado-personas/src/app/app.component.ts
+++ listado-personas/src/app/app.component.ts
@@ -4,13 +4,22 @@ import { Persona } from './persona.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   titulo = 'Listado de Personas.';
   personas: Persona[] = [
     new Persona('Juan', 'Pérez'),
     new Persona('Pedro', 'Pérez'),
-    new Persona('Jose', 'Pérez')
+    new Persona('Jose', 'Pérez'),
   ];
+  nombreInput: string = '';
+  apellidoInput: string = '';
+
+  agregarPersona() {
+    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+    this.personas.push(persona1);
+    this.nombreInput = '';
+    this.apellidoInput = '';
+  }
 }
