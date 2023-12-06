634c60042adaaf0d770d3dd1230cbe16cd58c371
RoCalderon1405 <rocalderon1405@hotmail.com>
2023-12-06 13:16:57 -0600

    Clase 37 property binding entre componentes

--- listado-personas/src/app/app.module.ts
+++ listado-personas/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { PersonaComponent } from './persona/persona.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      PersonaComponent
+   ],
   imports: [
     BrowserModule,
     FormsModule
