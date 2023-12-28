d2b1c804de1de95efc7c8c303ce4306428d9454f
RoCalderon <rocalderon1405@hotmail.com>
2023-12-27 20:37:10 -0600

    Clase 46

--- listado-personas/src/app/personas.service.ts
+++ listado-personas/src/app/personas.service.ts
@@ -1,5 +1,8 @@
+import { Injectable } from '@angular/core';
 import { Persona } from './persona.model';
+import { LoggingService } from './LoggingService.service';
 
+@Injectable()
 export class personasService {
   personas: Persona[] = [
     new Persona('Juan', 'Pérez'),
@@ -7,7 +10,10 @@ export class personasService {
     new Persona('Jose', 'Pérez'),
   ];
 
+  constructor(private logginService: LoggingService) {}
+
   personaAgregada(persona: Persona) {
+    this.logginService.enviaMensajeAConsola(`Agregamos persona ${persona.nombre}  ${persona.apellido}`)
     this.personas.push(persona);
   }
 }
