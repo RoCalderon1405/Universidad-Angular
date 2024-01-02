297d05bac7c1f3e1b3ed975e90f5240dbfaed440
RoCalderon1405 <rocalderon1405@hotmail.com>
2024-01-02 16:47:18 -0600

    Clase 69 Manejo errores de ruta

--- listado-personas/src/app/app-routing.module.ts
+++ listado-personas/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { PersonasComponent } from './personas/personas.component';
 import { FormularioComponent } from './personas/formulario/formulario.component';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
   { path: '', component: PersonasComponent },
@@ -13,6 +14,10 @@ const routes: Routes = [
       { path: 'agregar', component: FormularioComponent },
       { path: ':id', component: FormularioComponent },
     ],
+  },
+  {
+    path: '**',
+    component: ErrorComponent,
   },
 ];
 
